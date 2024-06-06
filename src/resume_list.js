2de5b1ba3d54e762260bed23826865139613ef38
송선우 <sun1210050@naver.com>
2024-06-06 23:35:54 +0900

    seonwoo:

--- src/resume_list.js
+++ src/resume_list.js
@@ -1,8 +1,39 @@
 import { TopBar } from './Homepage.js';
+import { AiOutlineFileText, AiOutlineFolderOpen, AiOutlineSignature, AiOutlineUpload } from "react-icons/ai";
+import React from 'react';
+import styles from './resume_list.module.css';
 
 function Page() {
     return (
-        <TopBar selected="자기소개서" login={false} />
+        <div className={styles.test}>
+            <TopBar selected="자기소개서" login={false} />
+            <div className={styles.iconButtons}>
+                <button className={`${styles.buttonWithIcon} ${styles.buttonHover}`}>
+                    <div className={styles.iconAndText}>
+                        <AiOutlineFileText size={35} className={`${styles.icon} ${styles.iconHover}`} />
+                        <span className={`${styles.buttonText} ${styles.buttonTextHover}`}>자소서목록</span>
+                    </div>
+                </button>
+                <button className={`${styles.buttonWithIcon} ${styles.buttonHover}`}>
+                    <div className={styles.iconAndText}>
+                        <AiOutlineFolderOpen size={35} className={`${styles.icon} ${styles.iconHover}`} />
+                        <span className={`${styles.buttonText} ${styles.buttonTextHover}`}>불러오기</span>
+                    </div>
+                </button>
+                <button className={`${styles.buttonWithIcon} ${styles.buttonHover}`}>
+                    <div className={styles.iconAndText}>
+                        <AiOutlineSignature size={35} className={`${styles.icon} ${styles.iconHover}`} />
+                        <span className={`${styles.buttonText} ${styles.buttonTextHover}`}>새 자소서</span>
+                    </div>
+                </button>
+                <button className={`${styles.buttonWithIcon} ${styles.buttonHover}`}>
+                    <div className={styles.iconAndText}>
+                        <AiOutlineUpload size={35} className={`${styles.icon} ${styles.iconHover}`} />
+                        <span className={`${styles.buttonText} ${styles.buttonTextHover}`}>내보내기</span>
+                    </div>
+                </button>
+            </div>
+        </div>
     );
 }
 
